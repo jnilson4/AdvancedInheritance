70d5a808c9d8e9c1f6f1c8bb4e4f4ac0f9e4d8df
Jake Nilson <j.nilson132@gmail.com>
2017-04-10 13:20:12 -0600

    Added the interface.

---
+++ src/inheritance/model/Cody.java
@@ -0,0 +1,14 @@
+package inheritance.model;
+
+/**	
+ * @author jake.nilson
+ * @author jo.alabudi
+ */
+
+public interface Cody
+{
+	public String teach(String knowledge);
+	public String codeEverything(String code);
+	public int speakSpeed(int speed);
+	public boolean isCooking();
+}
---
+++ src/inheritance/model/UltimateTeacher.java
@@ -0,0 +1,11 @@
+package inheritance.model;
+
+public abstract class UltimateTeacher implements Cody
+{
+	private String knowledge;
+	private String code;
+	private int speed;
+	private boolean isCooking;
+	
+	
+}
