6c0f39cdd84985637b6359dfe4e6e79cab4f3f2b
Jake Nilson <j.nilson132@gmail.com>
2017-04-10 13:50:08 -0600

    Added the abstract class getters.

--- src/inheritance/model/UltimateTeacher.java
+++ src/inheritance/model/UltimateTeacher.java
@@ -7,5 +7,67 @@ public abstract class UltimateTeacher implements Cody
 	private int speed;
 	private boolean isCooking;
 	
+	public UltimateTeacher()
+	{
+		this.knowledge = "";
+		this.code = "";
+		this.speed = 0;
+		this.isCooking = false;
+	}
 	
+	public String getKnowledge(String knowledge)
+	{
+		if(knowledge.contains("I know"))
+		{
+			System.out.println("YOU KNOW STUFF!");
+		}
+		else
+		{
+			System.out.println("YOU DON'T KNOW STUFF!");
+		}
+		
+		return knowledge;
+	}
+
+	public String getCode(String code)
+	{
+		if(code.contains("if"))
+		{
+			System.out.println("YOU USED AN IF STATEMENT!");
+		}
+		else if(code.contains("else"))
+		{
+			System.out.println("YOU USED AN ELSE STATEMENT!");
+		}
+		
+		return code;
+	}
+
+	public int getSpeed(int speed)
+	{	
+		if(speed >= 10)
+		{
+			System.out.println("You are fast like Cody!");
+		}
+		else
+		{
+			System.out.println("Sorry, you are slower than Cody!");
+		}
+		
+		return speed;
+	}
+
+	public boolean isCooking()
+	{
+		if(isCooking)
+		{
+			System.out.println("Youre cooking!");
+		}
+		else
+		{
+			System.out.println("Go cook!");
+		}
+		
+		return isCooking;
+	}
 }
